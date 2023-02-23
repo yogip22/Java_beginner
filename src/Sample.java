403a650cff11684f78f9c06178ccd38c62dbfda9
yogip <yogeshprabhu9999@gmail.com>
2023-02-24 00:38:39 +0530

    Understanding primitive and reference types

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,14 @@
+import java.awt.*;
+import java.util.Date;
 
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int age = 30; //primitive type
+        Date now = new Date(); //reference type
+        System.out.println(age);
+        System.out.println(now.getTime()); //getTime() methods returns the time component of the now object
+        Point point1 = new Point();
+        System.out.println(point1);
     }
 }
